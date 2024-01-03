36963bba2d40a0efd2fc5b89804304e0f73221d3
M Mohsin Sackeer <mohsin.ms97@gmail.com>
2024-01-03 20:47:58 +0530

    Edited Contact Me

--- src/Components/Body/ContactMe/ContactMeComponents.js
+++ src/Components/Body/ContactMe/ContactMeComponents.js
@@ -13,7 +13,6 @@ const ContactDetailsDiv = ({data,copySuccessMessage}) => {
         <div style={contactMeStyles.contactDetailsDiv}>
 
             <p style={contactMeStyles.headerP}>CONTACT DETAILS</p>
-            <PhoneNumberCard data={data} copySuccessMessage={copySuccessMessage}/>
             <EmailAddressCard data={data}copySuccessMessage={copySuccessMessage}/>
 
         </div>
@@ -31,19 +30,7 @@ const SocialNetworkDetailsDiv = ({data}) => {
     )
 }
 
-const PhoneNumberCard = ({data,copySuccessMessage}) => {
 
-    return (
-        <div className="copyContactDiv" 
-             style={contactMeStyles.contactDetailsCardDiv}
-             onClick={()=>copyTextToClipBoard(data.contactDetails.phoneNumber.value, 'phone number', copySuccessMessage.copySuccessMessage)}>
-
-            <img src={require("../../../assets/phone-icon.png")} style={{height:40,width:'auto'}}/>
-            <p style={contactMeStyles.labelP}>Copy Phone Number</p>
-
-        </div>
-    )
-}
 
 const EmailAddressCard = ({data,copySuccessMessage}) => {
     return(
@@ -51,7 +38,7 @@ const EmailAddressCard = ({data,copySuccessMessage}) => {
              style={contactMeStyles.contactDetailsCardDiv} 
              onClick={()=>copyTextToClipBoard(data.contactDetails.emailAddress.value, 'email address', copySuccessMessage.copySuccessMessage)}>
                 
-                <img src={require("../../../assets/email-icon.png")}
+                <img src={data.contactDetails.emailAddress.imageUrl}
                      style={{height:'auto',width:55}}/>
                 <p style={contactMeStyles.labelP}>Copy Email Address</p>
         </div>
@@ -67,7 +54,7 @@ const LinkedInCard = ({data}) => {
     return (
         <div style={contactMeStyles.socialNetworkCardDiv}>
             <a href={data.socialMediaContacts.linkedIn.websiteUrl} target="_blank" rel="noreferrer noopener">
-                <img src={require("../../../assets/linkedin-icon.png")} 
+                <img src={data.socialMediaContacts.linkedIn.imageUrl} 
                      style={{height: 40, width:'auto',zoom:'110%'}}/>
             </a>
             <p style={contactMeStyles.labelP}>LinkedIn</p>
@@ -80,7 +67,7 @@ const GithubCard = ({data}) => {
     return (
         <div style={contactMeStyles.socialNetworkCardDiv}>
             <a href={data.socialMediaContacts.github.websiteUrl} target="_blank" rel="noreferrer noopener">
-                <img src={require("../../../assets/github-icon.png")} style={{height: 40, width:'auto'}}/>
+                <img src={data.socialMediaContacts.github.imageUrl} style={{height: 40, width:'auto'}}/>
             </a>
             <p style={contactMeStyles.labelP}>GitHub</p>
         </div>
